80ced7467d4076eef08571c0a835001ba66bb3e9
Elijah Gutman <Gutmane7@gmail.com>
2024-06-16 11:17:52 -0400

    adding some content

--- practice.rb
+++ practice.rb
@@ -1,3 +1,4 @@
+# write a whike statement making the code display positive numbers from 1-10
 number = 1
 
 while number <= 10
@@ -6,3 +7,9 @@ while number <= 10
 
     end
 
+    # write a if statement with correct syntax 
+if number < 2
+    puts "big Ole Number"
+else number == 1
+    puts "this is simple code"
+end
