19e392a4d8df7955599db14b74117fc2cfb62102
Elijah Gutman <Gutmane7@gmail.com>
2024-06-17 15:50:49 -0400

    Trial and error

---
+++ 211.rb
@@ -0,0 +1,31 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+
+#Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+
+
+trilogies = ["Star wars", "Lord of the rings", "Indiana Jones"]
+
+trilogies.push ("The Godfather")
+trilogies.push ("Mad Max")
+
+
+pp trilogies
+
+# Were you able to easily solve the problem from memory?
+# I had it almost correct a series of times I had it as trilogies.push = ["the god father"]
+# Took me having to read the error and look at the answer a few times before I realized i didn't need the equal sign
+#and i had to change the type of parenthesis i was using
+
+# If so, describe the next skill you will start to practice tomorrow. 
+# The skill I need to work on is really just being precise, even after staring at the answer for a couple minutes
+# I couldn't figure out what was wrong until i copied and pasted it into the same terminal
+# What i learned is why all the devs i know use multiple screens instead of tabbing in and out of windows 
+# If not, describe the part you got stuck on that requires more practice.
+# I am actually pretty good at remembering the details from memory I just suffer from the same issue i had in college
+# I would do the math equation perfectly then subract instead of add a negative number
+# I would write a perfect paper with spelling errors the average third grade could catch, aka a lack of prescision 
