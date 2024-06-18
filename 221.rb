a939576af0148ed113edfafa211459a133c05ab6
Elijah Gutman <Gutmane7@gmail.com>
2024-06-18 15:21:58 -0400

    simple enough

---
+++ 221.rb
@@ -0,0 +1,12 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Write a while loop that prints the word "hello" 5 times.
+
+greeting = 1
+
+while greeting <= 5
+    puts "hello"
+    greeting = greeting + 1
+end
