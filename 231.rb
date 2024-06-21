adc09414487012b22cb4ea639aadf40a8a104bf5
Elijah Gutman <Gutmane7@gmail.com>
2024-06-21 15:25:19 -0400

    edits

---
+++ 231.rb
@@ -0,0 +1,21 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a method that takes in a number and returns the number times two. 
+# Then run the method and print the result.
+
+def double(number)
+    return number * 2
+end
+  
+puts double(5)
+
+# Were you able to easily solve the problem from memory?
+# Not at all I had completly forgotten what a method was and how to write one
+# I knew I had forgotten though and thats why I chose this particular exersise  to work
+# on my weaknesses and further humble myself as if this whole process wasn't enough 
+
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# I intend to do more  basic work with methods as a means of working further on this 
