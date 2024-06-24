f8fd58bb7e012153f4e8e883f78f13ba3ece53dd
Elijah Gutman <Gutmane7@gmail.com>
2024-06-24 14:25:00 -0400

    edits

---
+++ 241.rb
@@ -0,0 +1,10 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a method that takes in a string and returns the string with all capital 
+# letters. Then run the method and print the result.
+def example(word)
+    word.upcase
+end
+puts example("hello")
