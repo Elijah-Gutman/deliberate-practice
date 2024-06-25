2358a179f5ab5c0bcc3dec616eb5376470143a02
Elijah Gutman <Gutmane7@gmail.com>
2024-06-25 15:06:32 -0400

    edits

---
+++ 251.rb
@@ -0,0 +1,22 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a method that takes in two numbers and returns the first number subtracted 
+# by the second. Then run the method and print the result.
+
+
+def example(number1, number2)
+    return number1 - number2
+end
+
+pp example(3,2)
+
+
+# Were you able to easily solve the problem from memory?
+# Not even close
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# as suspected my memory of methods has gaps so I will continue to work on them
+# I actually intend to do this exact same one as my methodology when trying to do it
+# from memory was highy suspect and definitly needs work
