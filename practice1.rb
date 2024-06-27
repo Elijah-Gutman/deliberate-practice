15dbf488b31eff531a6b35924370bd2c2d0996b0
Elijah Gutman <Gutmane7@gmail.com>
2024-06-27 14:42:14 -0400

    edits

---
+++ practice1.rb
@@ -0,0 +1,8 @@
+# Write a method that takes in a number and returns the number times itself. 
+# Then run the method and print the result.
+
+def math(number1)
+    return number1 * number1
+end
+
+pp math(3)
