1fa72a146c51e6ef392db88abbbfe7a8d6b7cdb9
Elijah Gutman <Gutmane7@gmail.com>
2024-07-03 14:51:22 -0400

    Add exercise solutions

---
+++ 321.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#  Write a while loop that prints the even numbers from 2 to 40.
+number = 0
+while number < 40
+    number = number + 2
+    puts number
+end
+
+# Were you able to easily solve the problem from memory? 
+# for once Yes i actually was 
+# If so, describe the next skill you will start to practice tomorrow.
+#  I am going to loop back to working on conditionals as its been the most
+#amount of time since I have worked on them
+# If not, describe the part you got stuck on that requires more practice.
