2d6e6c4122b69aee9aa5fc9761ddbd5713989db6
Elijah Gutman <Gutmane7@gmail.com>
2024-07-15 15:56:17 -0400

    edits

---
+++ 351.rb
@@ -0,0 +1,26 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# 
+
+# Write a Location class with attributes for latitude and longitude.
+
+class Location
+
+  def initialize(longitude, latitude)
+  @longitude = longitude
+  @latitude = latitude
+  end
+end
+
+location = Location.new(41.23,34.45)
+pp location
+
+# Were you able to easily solve the problem from memory?
+# No but I at least had the idea inside of my head this time,
+# I had to rely extensivly on the material to make this work 
+# they are simple but i've done very few of these so I am working on remembering 
+# the syntax
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# The syntax, so i will keep working on unique methods until i get the syntax down
