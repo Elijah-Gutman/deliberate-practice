66cca4cdb43cbcb72834612cc17b5eedaca55901
Elijah Gutman <Gutmane7@gmail.com>
2024-07-19 15:55:27 -0400

    edits

---
+++ 421.rb
@@ -0,0 +1,21 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# 
+# Write a method that takes in three strings and returns a string that combines all 
+# three strings with spaces in 
+# between. Then run the method and print the result.
+ def interpolate(word1, word2, word3)
+  return word1 + " " + word2 + " " + word3
+   
+ end
+pp interpolate("I", "am" , "here")
+
+# Were you able to easily solve the problem from memory?
+# No, i had to look at some previous examples but I learned a lot from this
+# particular practice in both syntax and how interpolation works
+# when using methods
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# 
+#Im going to keep working on methods, I am improving  my memory of syntax
