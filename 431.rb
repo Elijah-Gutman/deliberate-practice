8f16e832bb6ac0b0281c6e747e3328636ba400a7
Elijah Gutman <Gutmane7@gmail.com>
2024-07-22 19:25:04 -0400

    edits

---
+++ 431.rb
@@ -0,0 +1,16 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# 
+ # Write a method that takes in a number and returns the number times 10 plus 30.
+ # Then run the method and print the result.
+ def math(number)
+   number * 10 + 30
+ end
+pp math(10)
+
+# Were you able to easily solve the problem from memory?
+# for the first time yes
+# If so, describe the next skill you will start to practice tomorrow.
+# going to advance futher with more complicated methods as its STILL my weakest point
+# If not, describe the part you got stuck on that requires more practice.
