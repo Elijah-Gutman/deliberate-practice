8539ee5caf155e761fff1ad205a92fa995b14d93
Elijah Gutman <Gutmane7@gmail.com>
2024-07-24 19:17:37 -0400

    edits

---
+++ 441.rb
@@ -0,0 +1,28 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write a Song class with attributes for name, artist, and duration.
+# 
+
+ class Song
+   def initialize(name, artist, duration)
+ 
+   
+  @artist = artist
+  @name = name
+  @duration = duration
+
+ end
+end
+
+song = Song.new("Califorication", "Red Hot Chilli Peppers", "5:22")
+pp song
+
+
+# Were you able to easily solve the problem from memory?
+# I was pretty darn close, just basic syntax error
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# I need to keep working on classes as I am close to having them entirely in my memory bank
+# The issue was remembering where "def" came in as i had forgotten it was inside the class
+# block of code and not in its own sepetated from it
--- practice1.rb
+++ practice1.rb
@@ -11,3 +11,13 @@ pp math(3)
 # the multiplication would occur in the pp line something to the effect 
 # of pp (math)(number1 * number1) and i realized afterwards their would be no value for
 #  number1
+class Song
+    def initialize(name, artist, duration)
+      @name = name
+      @artist = artist
+      @duration = duration
+    end
+  end
+  
+  song = Song.new("Gloria", "Patti Smith", "5:56")
+  pp song
