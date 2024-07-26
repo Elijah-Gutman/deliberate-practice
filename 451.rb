9e13b1d1ae6b247b0e363afd356141f92e0d412e
Elijah Gutman <Gutmane7@gmail.com>
2024-07-26 14:49:51 -0400

    edits

---
+++ 451.rb
@@ -0,0 +1,24 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# Write a Rectangle class with attributes for width and height.
+class Rectangle
+  def initialize(height, width)
+  @height = height
+  @width = width
+  
+  end
+end
+rectangle = Rectangle.new(10, 20)
+pp rectangle
+
+
+# Were you able to easily solve the problem from memory?
+# i was close its all their in my head, the syntax is even in my head now
+# i just keep forgetting the order in which it all goes
+# also the way the deliberate practice phrases the question i keep forgetting to actually input
+# any values into the attributes as it doesn't specifically ask
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+# I'm going to keep working on classes as I have gotten methods down and they
+# are just slightly more complicated versions of the same thing
