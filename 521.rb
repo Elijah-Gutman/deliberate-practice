6dba993e6eb3e8f2a9400f1ee11106c7b7c1569b
Elijah Gutman <Gutmane7@gmail.com>
2024-07-29 19:50:41 -0400

    edits

---
+++ 521.rb
@@ -0,0 +1,21 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+# 
+#  Write an Account class with attributes for name and balance.
+class Account
+  def initialize(name, balance)
+    @name = name
+    @balance = balance
+  end
+end
+account = Account.new("Jeff Bezos", "lots of $$$")
+pp account
+
+
+# Were you able to easily solve the problem from memory?
+# for the first time i was 95% of the way their i just second guessed myself at the last second
+# If so, describe the next skill you will start to practice tomorrow.
+# I will move onto more advanced versions of the same thing with attr reader and attr writer
+# methods and attributes that perform more complicated actions 
+# If not, describe the part you got stuck on that requires more practice.
